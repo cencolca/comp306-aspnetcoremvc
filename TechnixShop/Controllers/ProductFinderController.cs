dd49d8f62acf4ed2fc80e50ac0e524475d70e94c
Shafiq <shafiqu@my.centennialcollege.ca>
2018-12-15 20:54:04 -0500

    Routes to cart/buy/1 - BUT View page db is null in ProductFinder!!

---
+++ TechnixShop/Controllers/ProductFinderController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TechnixShop.Data;
+using TechnixShop.Models;
+
+namespace TechnixShop.Services
+{
+    public class ProductFinder
+    {
+        private readonly ApplicationDbContext db;
+        public ProductFinder()
+        {
+
+        }
+        internal Product find(string id)
+        {
+            var product = db.Product
+                            .Where(p => p.Id.Equals(id))
+                            .FirstOrDefault();
+            return product;
+        }
+
+        //internal dynamic findAll()
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+}
